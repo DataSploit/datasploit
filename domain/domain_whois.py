c62084b6cd914d198022d7328f93d633aa7d3300
Edznux <edznux@gmail.com>
2017-10-27 20:01:53 +0000

    Add domain_whois json output

--- domain/domain_whois.py
+++ domain/domain_whois.py
@@ -1,40 +1,58 @@
 #!/usr/bin/env python
 
+import json
 import base
 import sys
 import whois
 from termcolor import colored
 import time
+import datetime
 
 ENABLED = True
-
+OUTPUT_TYPE = "console"
 
 class style:
     BOLD = '\033[1m'
     END = '\033[0m'
 
-
 def whoisnew(domain):
     w = whois.whois(domain)
     return dict(w)
 
-
 def banner():
-    print colored(style.BOLD + '---> Finding Whois Information.' + style.END, 'blue')
-
+    if(OUTPUT_TYPE == "console"):
+        print colored(style.BOLD + '---> Finding Whois Information.' + style.END, 'blue')
 
 def main(domain):
     return whoisnew(domain)
 
-
 def output(data, domain=""):
-    print data
-    print "\n-----------------------------\n"
 
+    if(OUTPUT_TYPE == "console"):
+        print data
+        print "\n-----------------------------\n"
+
+    if(OUTPUT_TYPE == "json"):
+        js = {}
+        for i in data:
+            # convert iterables to json list
+            if(hasattr(data[i], '__iter__')):
+                js[i] = list()
+                for j in data[i]:
+                    js[i].append(j)
+            # convert datetime to string
+            elif (isinstance(data[i], datetime.datetime)):
+                js[i] = data[i].strftime("%s")
+            else:
+                js[i] = str(data[i])
+        
+        print json.dumps(js)
 
 if __name__ == "__main__":
     try:
         domain = sys.argv[1]
+        if(len(sys.argv) > 2):
+             OUTPUT_TYPE=sys.argv[2]
         banner()
         result = main(domain)
         output(result, domain)
