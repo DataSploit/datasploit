f2ba8021176db54b08a124537bf36fcd82ab371b
GI_Jack <iamjacksemail@hackermail.com>
2018-09-08 15:13:45 -0400

    changed shebangs to python2

--- ip/ip_whois.py
+++ ip/ip_whois.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 
 from ipwhois import IPWhois
 import sys
--- username/username_traviscidetails.py
+++ username/username_traviscidetails.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 
 # Credits: https://github.com/int0x80/tcispy
 
