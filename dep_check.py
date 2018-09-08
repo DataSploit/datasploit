0517ea3de12b941891cfeecab4f6808b0c57df98
GI_Jack <iamjacksemail@hackermail.com>
2018-09-08 13:59:31 -0400

    * ported from pip.get_installed_distributions() to   pkg_resources.working_set.entry_keys . This is how upstream wants this done   and still works on python * now lists missing modules

--- dep_check.py
+++ dep_check.py
@@ -1,4 +1,4 @@
-import pip
+import pkg_resources
 import sys
 
 def check_dependency():
@@ -8,13 +8,18 @@ def check_dependency():
     with open('requirements.txt') as f:
         list_deps = f.read().splitlines()
 
-    pip_list = sorted([(i.key) for i in pip.get_installed_distributions()])
+    # get list of installed python packages
+    pip_list = []
+    for item in pkg_resources.working_set.entry_keys:
+        pip_list += pkg_resources.working_set.entry_keys[item]
 
     for req_dep in list_deps:
         if req_dep not in pip_list:
             missing_deps.append(req_dep)
 
     if missing_deps:
-        print "You are missing a module for Datasploit. Please install them using: "
-        print "pip install -r requirements.txt"
+        print "You are missing a module for Datasploit:"
+        for module in missing_deps:
+            print module
+        print "Please install them using: pip install -r requirements.txt"
         sys.exit()
