ba688f7eeb6672644bf6d602130db42b28a7b65a
Michael R. Lawrence <mike@michaellawrenceit.com>
2019-06-13 21:13:51 -0500

    Create setup.py
    create template setup.py
    https://gist.github.com/philippeowagner/6826498 via autogent toy.

---
+++ setup.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name='',
+    version=__import__("").__version__,
+    packages=find_packages(),
+    include_package_data=False,
+    install_requires=["importlib","sys","pip","requests"],
+)
