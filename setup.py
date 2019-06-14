1edd8b7d2504cce2cfbf4d588acd917251f14b61
Michael L <mike@michaellawrenceit.com>
2019-06-13 21:22:02 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
 # -*- coding: utf-8 -*-
+# https://gist.github.com/philippeowagner/6826498 autogen
+#https://stackoverflow.com/questions/14399534/reference-requirements-txt-for-the-install-requires-kwarg-in-setuptools-setup-py 
+# https://wiki.gentoo.org/wiki/Project:Python/distutils-r1 makes ebuilds for Gentoo or Pentoo and or Arch etc too easy. 
+## proffer the template for author and improvemnts. 
+
 from setuptools import setup, find_packages
 
 setup(
