3a117a3f24db88e7fc2454a26f653174a400c5e0
Ryan Payton <>
2020-05-22 20:26:10 -0700

    moving style class

--- domain/domain_GooglePDF.py
+++ domain/domain_GooglePDF.py
@@ -1,18 +1,13 @@
 #!/usr/bin/env python
 
-from . import base
+import base
 import sys
 import urllib.request, urllib.error, urllib.parse
 import re
 import string
 from termcolor import colored
 
-ENABLED = False
-
-
-class style:
-    BOLD = '\033[1m'
-    END = '\033[0m'
+ENABLED = True
 
 
 def googlesearch(query, ext):
@@ -28,10 +23,10 @@ def googlesearch(query, ext):
     }
     req = urllib.request.Request(getrequrl, headers=hdr)
     response = urllib.request.urlopen(req)
-    data = response.read()
+    data = response.read().decode('UTF-8')
     data = re.sub('<b>', '', data)
     for e in ('>', '=', '<', '\\', '(', ')', '"', 'http', ':', '//'):
-        data = string.replace(data, e, ' ')
+        data = str.replace(data, e, ' ')
 
     r1 = re.compile('[-_.a-zA-Z0-9.-_]*' + '\.' + ext)
     res = r1.findall(data)
@@ -39,7 +34,7 @@ def googlesearch(query, ext):
 
 
 def banner():
-    print(colored(style.BOLD + '\n---> Searching Google for domain results\n' + style.END, 'blue'))
+    print(colored(base.style.BOLD + '\n---> Searching Google for domain results\n' + base.style.END, 'blue'))
 
 
 def main(domain):
